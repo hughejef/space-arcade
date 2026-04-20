aa144e3c2bc6aefd92e1b2bcbbb5b83f53036d6c
Prabhash Sharma <b-a-s-h@Prabhashs-MacBook-Pro.local>
2026-04-20 19:28:43 -0400

    Added player ship sprites with wings, cockpit, and glow effects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,94 @@ function App() {
     canvas.width = 800
     canvas.height = 600
 
+    const player1 = { x: 380, y: 40, width: 40, height: 44 }
+    const player2 = { x: 380, y: 516, width: 40, height: 44 }
+
+    function drawShip(x, y, w, h, color, facingUp) {
+      ctx.shadowColor = color
+      ctx.shadowBlur = 12
+      ctx.fillStyle = color
+
+      if (facingUp) {
+        ctx.beginPath()
+        ctx.moveTo(x + w / 2, y)
+        ctx.lineTo(x + w / 2 + 6, y + 14)
+        ctx.lineTo(x + w / 2 - 6, y + 14)
+        ctx.closePath()
+        ctx.fill()
+        ctx.fillRect(x + w / 2 - 8, y + 14, 16, 18)
+        ctx.beginPath()
+        ctx.moveTo(x + w / 2 - 8, y + 16)
+        ctx.lineTo(x, y + h)
+        ctx.lineTo(x + w / 2 - 8, y + 30)
+        ctx.closePath()
+        ctx.fill()
+        ctx.beginPath()
+        ctx.moveTo(x + w / 2 + 8, y + 16)
+        ctx.lineTo(x + w, y + h)
+        ctx.lineTo(x + w / 2 + 8, y + 30)
+        ctx.closePath()
+        ctx.fill()
+        ctx.fillStyle = '#ffffff44'
+        ctx.beginPath()
+        ctx.arc(x + w / 2, y + 18, 4, 0, Math.PI * 2)
+        ctx.fill()
+        ctx.fillStyle = color + '66'
+        ctx.fillRect(x + w / 2 - 5, y + h, 10, 4)
+      } else {
+        ctx.beginPath()
+        ctx.moveTo(x + w / 2, y + h)
+        ctx.lineTo(x + w / 2 + 6, y + h - 14)
+        ctx.lineTo(x + w / 2 - 6, y + h - 14)
+        ctx.closePath()
+        ctx.fill()
+        ctx.fillRect(x + w / 2 - 8, y + 12, 16, 18)
+        ctx.beginPath()
+        ctx.moveTo(x + w / 2 - 8, y + h - 16)
+        ctx.lineTo(x, y)
+        ctx.lineTo(x + w / 2 - 8, y + 14)
+        ctx.closePath()
+        ctx.fill()
+        ctx.beginPath()
+        ctx.moveTo(x + w / 2 + 8, y + h - 16)
+        ctx.lineTo(x + w, y)
+        ctx.lineTo(x + w / 2 + 8, y + 14)
+        ctx.closePath()
+        ctx.fill()
+        ctx.fillStyle = '#ffffff44'
+        ctx.beginPath()
+        ctx.arc(x + w / 2, y + h - 18, 4, 0, Math.PI * 2)
+        ctx.fill()
+        ctx.fillStyle = color + '66'
+        ctx.fillRect(x + w / 2 - 5, y - 4, 10, 4)
+      }
+
+      ctx.shadowColor = 'transparent'
+      ctx.shadowBlur = 0
+    }
+
     function draw() {
       ctx.fillStyle = '#0a0a2e'
       ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+      // stars
+      ctx.fillStyle = '#ffffff'
+      for (let i = 0; i < 50; i++) {
+        const sx = (i * 137 + 29) % canvas.width
+        const sy = (i * 211 + 43) % canvas.height
+        ctx.fillRect(sx, sy, 1.5, 1.5)
+      }
+
+      // player labels
+      ctx.font = '12px monospace'
+      ctx.textAlign = 'center'
+      ctx.fillStyle = '#00ff88'
+      ctx.fillText('PLAYER 1', player1.x + player1.width / 2, player1.y - 8)
+      ctx.fillStyle = '#ff4466'
+      ctx.fillText('PLAYER 2', player2.x + player2.width / 2, player2.y + player2.height + 16)
+
+      drawShip(player1.x, player1.y, player1.width, player1.height, '#00ff88', false)
+      drawShip(player2.x, player2.y, player2.width, player2.height, '#ff4466', true)
 
       requestAnimationFrame(draw)
     }
