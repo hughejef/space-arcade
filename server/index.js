81b492bb8cbed188191fb0c1adb7d35b20135dd0
Jeff Hughes <jeff.hughes@cakehome.com>
2026-04-21 11:31:55 -0700

    Server received client input over socket.io

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const Player = require('./game/Player.js');
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+// on connection, new Player(socket.id); playerList.set(socket.id, player)
+// on disconnect, playerList.delete(socket.id)
+const playerList = new Map();
+const allowedInput = ['left', 'right', 'shoot'];
 
 const app = express();
 const server = http.createServer(app);
@@ -13,9 +20,29 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log(`Client connected: ${socket.id}`);
+  const player = new Player(socket.id);
+  playerList.set(socket.id, player);
   socket.on('disconnect', () => {
     console.log(`Client disconnected: ${socket.id}`);
+    playerList.delete(socket.id);
   });
+
+  socket.on('input', (data) => {
+    //console.log(`input from ${socket.id}:`, data);
+    const player = playerList.get(socket.id);
+    if (!player) return;
+
+    // do nothing if valid key from list isn't pressed
+    if (!allowedInput.includes(data.key)) return;
+    if (data.state !== 'down' && data.state !== 'up') return;
+
+    if (data.key === "left") player.currentInput.left = (data.state === "down");
+    else if (data.key === "right") player.currentInput.right = (data.state === "down");
+    else if (data.key === "shoot") player.currentInput.shoot = (data.state === "down");
+    //console.log(`player state:`, player.currentInput);
+  });
+
+  
 });
 
 const PORT = process.env.PORT || 3001;
---
+++ server/test-client.js
@@ -0,0 +1,19 @@
+//https://socket.io/docs/v4/client-initialization/
+
+const { io } = require("socket.io-client");
+
+const socket = io('http://localhost:3001');
+
+socket.on('connect', () => {
+  console.log('connected as', socket.id);
+  socket.emit('input', { key: 'left', state: 'down' });
+  socket.emit('input', { key: 'right', state: 'down' });
+  socket.emit('input', { key: 'shoot', state: 'down' });
+  socket.emit('input', { key: 'cheat', state: 'down' }); // test fail
+  socket.emit('input', { key: 'left', state: 'removed' }); // test fail
+
+  setTimeout(() => {
+    socket.emit('input', { key: 'left', state: 'up' });
+    socket.disconnect();
+  }, 1000);
+});
