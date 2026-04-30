2ee2789f69d7b70021b6ce4ed6910c3fb2cac492
Jeffrey Hughes <hughejef@oregonstate.edu>
2026-04-30 12:21:48 -0700

    added tick look for server and test-client.js successfully tests player1/2 connecting, disconnecting, and room management

--- server/game/RoomManager.js
+++ server/game/RoomManager.js
@@ -3,35 +3,68 @@
 // developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/set
 
 const GameRoom = require('./GameRoom');
+const { TICK } = require('./constants');
 
 class RoomManager {
-    constructor(){
+    constructor() {
         this.roomMap = new Map();
+        this.tickIntervalId = null;
     }
 
-    generateCode(){
+    generateCode() {
         let code;
         code = Math.random().toString(36).substring(2, 6).toUpperCase();
         return code;
     }
 
-    createRoom(){
+    createRoom() {
         const code = this.generateCode();
         const room = new GameRoom(code);
         this.roomMap.set(code, room);
+        this.startTickLoop();
         return room;
     }
 
-    findRoom(code){
+    findRoom(code) {
         const room = this.roomMap.get(code);
-        if (room){
+        if (room) {
             return room;
         }
         return null;
     }
 
-    deleteRoom(code){
-        return this.roomMap.delete(code);
+    getAllRooms() {
+        const rooms = this.roomMap.values();
+        return rooms;
+    }
+
+    deleteRoom(code) {
+        this.roomMap.delete(code);
+        if (this.roomMap.size === 0) {
+            this.stopTickLoop();
+        }
+    }
+
+    oneTick(){
+    for (const room of this.roomMap.values()) {
+        room.tick();  // GameRoom.tick() will console.log for now
+    }
+    }
+
+
+    startTickLoop() {
+        if (this.tickIntervalId === null) {
+            this.tickIntervalId = setInterval(() => this.oneTick(), TICK.MS);
+        }
+    }
+
+    // https://stackoverflow.com/questions/109086/stop-setinterval-call-in-javascript
+    stopTickLoop() {
+        if (this.tickIntervalId !== null) {
+            clearInterval(this.tickIntervalId);
+            this.tickIntervalId = null;
+        }
+
     }
 }
 
