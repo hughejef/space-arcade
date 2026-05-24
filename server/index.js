1bc02926799e44befc97851b2835b5c2f6995bf2
Prabhash Sharma <b-a-s-h@Prabhashs-MacBook-Pro.local>
2026-05-24 10:21:11 -0400

    Restore cors middleware that was lost during server refactor

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
 const RoomManager = require('./game/RoomManager.js');
 const { TICK } = require('./game/constants.js');
 const { initializeDatabase, saveScore, getTopScores } = require('./Database.js');
 
-
 const allowedInput = ['left', 'right', 'shoot'];
 
 const app = express();
+app.use(cors()); // allow cross-origin HTTP requests so the deployed client can hit /leaderboard
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: '*' }
@@ -90,7 +91,6 @@ io.on('connection', (socket) => {
       // console.log('input set', slot, key, state);
     }
   });
-
 });
 
 const PORT = process.env.PORT || 3001;
